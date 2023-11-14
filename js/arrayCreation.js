0da3f91c29f4fb80180f92dc08367843a6a42f03
DavidAndBar <davidbar22@hotmail.com>
2023-11-14 13:23:23 -0500

    Adds game-over, right click feature to flag mines. Starts the recursive function to make possible to extends zeros when you click on a zero.

--- js/arrayCreation.js
+++ js/arrayCreation.js
@@ -1,4 +1,5 @@
 let minesGame;
+let minesDiscovered = 0;
 
 const createGame = (HEIGHT, WIDTH, MINES_PERCENTAGE) => {
 
@@ -52,31 +53,63 @@ const createGame = (HEIGHT, WIDTH, MINES_PERCENTAGE) => {
 }
 
 const clickMine = (event) => {
-    let rowNumber = event.target.parentElement;
-    let columnNumber = event.target;
-
-    if (!$("#select_mine")[0].checked) {
+    let rowNumber = event.target.parentElement.id;
+    let columnNumber = event.target.id;
+    if (event.which == 1 && $(`#${rowNumber} #${columnNumber}`).text() != "X") {
         checkCell(rowNumber, columnNumber);
-    } else {
-        if (!$(`#${rowNumber.id} #${columnNumber.id}`).text()) {
-            $(`#${rowNumber.id} #${columnNumber.id}`).text("X");
-        } else {
-            $(`#${rowNumber.id} #${columnNumber.id}`).text("");
-        }
+    } else if (event.which == 3) {
+        flagMine(rowNumber, columnNumber);
     }
     
 
 }
 
-const checkCell = (rowNumber, columnNumber) => {
-    let number = minesGame[rowNumber.id.slice(1)][columnNumber.id.slice(1)];
-    
-    $(`#${rowNumber.id} #${columnNumber.id}`).text(number);
+const flagMine = (rowNumber, columnNumber) => {
+    if (!$(`#${rowNumber} #${columnNumber}`).text()) {
+        $(`#${rowNumber} #${columnNumber}`).text("X");
+        minesDiscovered++;
+        $('#div-mines-discovered').text(minesDiscovered);
+    } else if ($(`#${rowNumber} #${columnNumber}`).text() == "X") {
+        $(`#${rowNumber} #${columnNumber}`).text("");
+        minesDiscovered--;
+        $('#div-mines-discovered').text(minesDiscovered);
+    }
 }
 
+const checkCell = (rowNumber, columnNumber) => { 
+    let number = minesGame[rowNumber.slice(1)][columnNumber.slice(1)];
+    if (number == -1) {
+        $("body").append('<div id="game-over-div"> You Lose </div>');
+        $("#game-over-div").css({
+            top: $("#mines-grid").position().top,
+            left: $("#mines-grid").position().left + parseFloat($("#mines-grid").css("margin-left")),
+            width: parseFloat($("#mines-grid").css("width")),
+            height: parseFloat($("#mines-grid").css("height"))
+        });
+    } else if (number == 0) {
+        $(`#${rowNumber} #${columnNumber}`).text(number);
+        openSurrondings(rowNumber, columnNumber); // here the logic to check cells around.
+    } else {
+        $(`#${rowNumber} #${columnNumber}`).text(number);
+    }
+}
 
+/*const openSurrondings = (rowNumber, columnNumber) => {
+    let i = rowNumber; // i height, j width
+    let k = columnNumber;
+    for (k = i-1; k <= i+1; k++) {
+        for(l = j - 1; l <= j + 1; l++){
+            if (!(k == i && l == j) && !(k==0 || l == 0) && !(k > HEIGHT || l > WIDTH)) { //I'm here, im missing to test the function and add the recursivity.
+                if (minesList[k][l] == 0) {
+                    countMines++;
+                }
+            }
+        }
+    }
+}*/
 
 $(document).ready(() => {
+    window.addEventListener("contextmenu", e => e.preventDefault());
     const HEIGHT = 10; // Beg: 10 Easy: 14 Inter: 20 Exp: 26
     const WIDTH = 8; // Beg: 8 Easy: 9 Inter: 15 Exp: 19
     const MINES_PERCENTAGE = 0.0875; // Beg: 7 Easy: 15 Inter: 40 Exp: 99
@@ -87,12 +120,15 @@ $(document).ready(() => {
         cssColumns = "";
         $("#mines-grid").append(`<div id="r${i}" class="rows"></div>`);
         for (let j = 1; j <= WIDTH; j++){
-            $(`#r${i}`).append(`<div id="c${j}" class="mine" ondblclick={clickMine(event)} onclick={clickMine(event)} ></div>`);
+            $(`#r${i}`).append(`<div id="c${j}" class="mine" ></div>`);
             cssColumns += "1fr ";
         }
         cssRows += "1fr ";
     }
 
+    $('.mine').bind("mousedown", (event) => {
+        clickMine(event);
+    })
     
 
     $("#mines-grid").css({
@@ -106,9 +142,3 @@ $(document).ready(() => {
     });
     console.log(minesGame);
 })
-
-console.log(minesGame);
-
-
-
-
